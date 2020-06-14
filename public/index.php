49480709c368b6611d23f35153200c6e77f464f5
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-06-14 15:44:01 -0300

    Db config

--- public/index.php
+++ public/index.php
@@ -1,1 +1,13 @@
-<h1>Hello World</h1>
+<?php
+
+require_once(dirname(__FILE__, 2) . '/src/config/database.php');
+
+$sql = 'SELECT * FROM users';
+
+//Database::getConnection();
+$result = Database::getResultFromQuery($sql);
+
+while($row = $result->fetch_assoc()) {
+    print_r($row);
+    echo '<br>';
+}
