3252ad1b4c396bcc0380c0710a709f10fd949763
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-06-16 15:10:45 -0300

    Login view

--- public/index.php
+++ public/index.php
@@ -1,12 +1,4 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . '/src/config/database.php');
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
-
-$user = new User(['name' => 'User', 'email' => 'user@email.com']);
-echo $user->get('id', 'name', 'email');
-
-foreach (User::get([], 'name') as $user) {
-    echo $user->$name;
-    echo '<br>'
-}
+require_once(dirname(__FILE__, 2) . '/src/views/login.php');
