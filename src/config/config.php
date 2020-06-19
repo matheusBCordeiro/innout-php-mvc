7265482887663f1ecc37a343e9ce545ef3ae088d
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-06-18 23:22:21 -0300

    Data generator

--- src/config/config.php
+++ src/config/config.php
@@ -3,6 +3,8 @@
 date_default_timezone_set('America/Sao_Paulo');
 setlocale(LC_TIME, 'pt_BR', 'pt_BR.uft-8', 'portuguese');
 
+define('DAILY_TIME', 60 * 60 * 8);
+
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
 define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
 define('TEMPLATE_PATH', realpath(dirname(__FILE__) . '/../views/template'));
