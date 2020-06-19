8960050a8a1eba05e92b3daad61df8666a412ac3
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-06-18 22:08:42 -0300

    Template

---
+++ src/views/template/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <span>Desenvolvido com</span>
+    <span><i class="icofont-heart text-danger mx-1"></i></span>
+    <span>por M<span class="text-danger">B</span>C</span>
+</footer>
+<script src="assets/js/app.js"></script>
+</body>
+</html>
