6a7edfc2b770ca3f8b8c4093c02a8958c06087ae
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-07-13 13:35:01 -0300

    Populating working hours

--- src/controllers/data_generator.php
+++ src/controllers/data_generator.php
@@ -1,7 +1,9 @@
 <?php
+loadModel('WorkingHours');
+Database::executeSQL('DELETE FROM working_hours');
+Database::executeSQL('DELETE FROM users WHERE id > 5');
 
-function getDayTemplateByOdds($regularRate, $extraRate, $lazyRate)
-{
+function getDayTemplateByOdds($regularRate, $extraRate, $lazyRate) {
     $regularDayTemplate = [
         'time1' => '08:00:00',
         'time2' => '12:00:00',
@@ -36,4 +38,26 @@ function getDayTemplateByOdds($regularRate, $extraRate, $lazyRate)
     }
 }
 
-print_r(getDayTemplateByOdds(10, 20, 70));
+function populateWorkingHours($userId, $initialDate, $regularRate, $extraRate, $lazyRate) {
+    $currentDate = $initialDate;
+    $today = new DateTime();
+    $columns = ['user_id' => $userId, 'work_date' => $currentDate];
+
+    while(isBefore($currentDate, $today)) {
+        if(!isWeekend($currentDate)) {
+            $template = getDayTemplateByOdds($regularRate, $extraRate, $lazyRate);
+            $columns = array_merge($columns, $template);
+            $workingHours = new WorkingHours($columns);
+            $workingHours->save();
+        }
+        $currentDate = getNextDay($currentDate)->format('Y-m-d');
+        $columns['work_date'] = $currentDate;
+    }
+}
+
+$lastMonth = strtotime('first day of last month');
+populateWorkingHours(1, date('Y-m-1'), 70, 20, 10);
+populateWorkingHours(3, date('Y-m-d', $lastMonth), 20, 75, 5);
+populateWorkingHours(4, date('Y-m-d', $lastMonth), 20, 10, 70);
+
+echo 'Tudo certo :)';
