247b43d6865dac565f380ee3f933af198fa3ae30
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-07-14 00:11:37 -0300

    Innout #2

--- src/views/template/messages.php
+++ src/views/template/messages.php
@@ -1,4 +1,19 @@
 <?php
+$errors = [];
+
+if(isset($_SESSION['message'])) {
+    $message = $_SESSION['message'];
+    unset($_SESSION['message']);
+} elseif($exception) {
+    $message = [
+        'type' => 'error',
+        'message' => $exception->getMessage()
+    ];
+
+    if(get_class($exception) === 'ValidationException') {
+        $errors = $exception->getErrors();
+    }
+}
 
 if ($exception) {
     $message = [
