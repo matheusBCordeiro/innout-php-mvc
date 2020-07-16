a6086f62b096ad14545525e3cdbbfc3d80050e3b
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-07-16 12:52:56 -0300

    User list

---
+++ src/controllers/users.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+requireValidSession(true);
+
+$users = User::get();
+foreach($users as $user) {
+    $user->start_date = (new DateTime($user->start_date))->format('d/m/Y');    
+    if($user->end_date) {
+        $user->end_date = (new DateTime($user->end_date))->format('d/m/Y');
+    }
+}
+
+loadTemplateView('users', [
+    'users' => $users,
+]);
